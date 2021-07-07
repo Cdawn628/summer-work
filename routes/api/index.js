99bdd223cae0af3d84966c6893f221e3000ed15b
Cdawn628 <75674719+Cdawn628@users.noreply.github.com>
2021-07-07 10:08:05 +0800

    Create index.js
    请求索引模块

---
+++ routes/api/index.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var handlebars = require("express3-handlebars").create({ defaultLayout: "main"});
+module.exports = function(app){
+
+  app.engine("handlebars", handlebars.engine); //设置视图引擎
+  app.set("view engine", "handlebars");
+
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false }))
+    
+    app.get('/',function(req,res){
+      res.send('hello world')
+    })
+
+    
+    //注册界面
+    //app.post('/api/register',require('./register'))
+   
+    //登陆界面
+    app.post('/api/login',require('./login'))
+
+   
+
+    // 测试路由
+    //app.get('/api/test',function(req, res){
+   //   res.render("thank-you")
+    //  })
+}

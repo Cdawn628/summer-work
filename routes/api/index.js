ca0eef96bfe1cfb69b1eba6d8c8d48c6225c0c0d
Cdawn628 <75674719+Cdawn628@users.noreply.github.com>
2021-07-10 10:07:48 +0800

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,14 @@ var express = require('express')
 var handlebars = require("express3-handlebars").create({ defaultLayout: "main"});
 module.exports = function(app){
 
+  
+  app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();
+});
+
   app.engine("handlebars", handlebars.engine); //设置视图引擎
   app.set("view engine", "handlebars");
 
